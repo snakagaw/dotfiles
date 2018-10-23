306ea4f0b3fd9bfb03ea4a20aeaef8d9e0a6c055
Nakagawa Sohei <sn1115poco@gmail.com>
2018-10-24 03:36:46 +0900

    zsh 関連の設定ファイルを追加

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/zsh/zlogin ~/.zlogin
+ln -sf ~/dotfiles/zsh/zlogout ~/.zlogout
+ln -sf ~/dotfiles/zsh/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
+ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
